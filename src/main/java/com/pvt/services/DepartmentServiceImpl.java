607c3755ab56afe27f063e73cb118c47d090306e
Администратор <@DELL>
2017-10-25 23:40:15 +0300

    Update README.md

---
+++ src/main/java/com/pvt/services/DepartmentServiceImpl.java
@@ -0,0 +1,19 @@
+package com.pvt.services;
+
+import com.pvt.DAO.DepartmentDAO;
+import com.pvt.model.Department;
+
+public class DepartmentServiceImpl implements DepartmentService{
+    
+    private DepartmentDAO departmentDAO;
+
+    public void setDepartmentDAO(DepartmentDAO departmentDAO) {
+        this.departmentDAO = departmentDAO;
+    }
+    //метод вставляет запись в таблицу, потом выбрасывает исключение,
+    //что приводит к откату транзакции
+    public void addDepartment(final Department dep)  {
+        departmentDAO.addDepartment(dep);
+        throw new RuntimeException("Exception throwed!");
+    }
+}
